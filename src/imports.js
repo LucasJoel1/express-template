05bb56d143bc3b2bedf2fef902712b5e55a84b04
William Kang <contact@williamk.ga>
2022-11-01 20:36:14 -0400

    update dependencies, make nodemon a dev dependency, remove dependencies that are built into node.js (fs, timers), remove http

--- src/imports.js
+++ src/imports.js
@@ -3,7 +3,6 @@ const express = require('express');
 module.exports = {
     express: require('express'),
     bodyParser: require('body-parser'),
-    http: require('http'),
     app: express(),
     fs: require('fs'),
 };
